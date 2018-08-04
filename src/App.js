668530ddae2942f361c4917e6eb2e832db7eac3c
yamato3310 <yamatotunasima1@gmail.com>
2018-08-04 17:06:50 +0900

    kinoutuika

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { component } from 'react';
+import TodoList from './TodoList';
+import './css/App.css'
+
+class App extends component {
+    constructor() {
+        super()
+        this.state = {
+            todo: [{
+                    id: 1,
+                    title: "Hello, React",
+                    desc: "React始めました",
+                    done: folse
+                },
+                {
+                    id: 2,
+                    title: "Hello, Redux!",
+                    desc: "Reduxも始めました",
+                    done: false
+                },
+            ]
+        }
+    }
+}
